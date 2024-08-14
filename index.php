568890af701848d5473759b84e77ad6687bd87da
Wilman Uriel Salmeron Martinez <mrwilman0553@gmail.com>
2024-08-13 19:59:58 -0600

    Creacion de index.php con estructura html

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>WILMAN URIEL SALMERON MARTINEZ</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css" />
+        <link rel="stylesheet" href="css/style.css" />
+        <script src ="jquery-3.7.1.min.js"></script>
+        <script src="js/boolstrap.bundle.min.js"></script>
+</head>
+<body>
+
+</body>
+<html>
