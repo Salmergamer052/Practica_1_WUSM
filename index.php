a98df5c5c630ecc24ccad87b46ef2b97514a38e4
Wilman Uriel Salmeron Martinez <mrwilman053@gmail.com>
2024-08-27 20:02:28 -0600

    Creado HTML y archivo CCS

--- index.php
+++ index.php
@@ -1,13 +1,52 @@
 <!DOCTYPE HTML>
-<html>
+<html lang="es">
     <head>
         <title>WILMAN URIEL SALMERON MARTINEZ</title>
+        <meta name="viemport" content="width=device-width, initial-scale=1"/>
+        <meta charset="UTF-8"/>
         <link rel="stylesheet" href="css/bootstrap.min.css" />
         <link rel="stylesheet" href="css/style.css" />
         <script src ="jquery-3.7.1.min.js"></script>
         <script src="js/boolstrap.bundle.min.js"></script>
 </head>
 <body>
+    <div class="container-fluid">
+        <div class="Contenedor">
+            <div class="row align-items-center h=100">
+                <div class= "col"> </div>
+                <div class= "col"> </div>
+                <div class="align-items-center">
+          </div>       <p>Wilman Uriel Salmeron Martinez</p>
+                       <form name="frm_login" id="frm_login" action=core/process.php>
+                       <table>
+                        <thead>
+                            <th>
+                                <th class="parrafo_centrado">Inicio de sesion</th>
+                           </th>
+                           <th>
+                            <th>&nbsp;</th>
+                          </th>
+                       </thead>
+                       <tbody>
+                           <tr>
+                            <div class="form-row">
+                                <div class="form-group col-md-6">
+                                    <label for="txt_usuario">USUARIO:</label>
+                                    <input type=text class="form-control" name="txt_usuario" maxlength="15"aria describeddy="txt_user_help" required/>
+                                    <small id="txt_user_help" class="form-text text_muted"> el usuario es obligatorio</small>
+                              </div>
+                              <div class="form-group dol-md-6">
+                                <label for="txt_password">CONTRASEÑA:</label>
+                                <input type="password" class="form-control" name="txt_password" id="txt_password" maxlength="15" aria-describeddy="txt_password_help" required  />
+                                <small id="txt_password>_help" class="form-text text-muted">Contraseña obligatoria</small>
+                                
+                              </div>
+                            </div>
+                          </tr>
+                    </table>
 
+        </div>
+     </div>
+   </div>
 </body>
 <html>
